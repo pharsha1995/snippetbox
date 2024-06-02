2b7d6d198f34d500c470692dd6035d6095cea9dd
harsha <pharshavardhan2@gmail.com>
2024-06-02 13:17:32 +0530

    add loginform and add non form specific errors

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -10,11 +10,12 @@ import (
 var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 type Validator struct {
-	FieldErrors map[string]string
+	NonFieldErrors []string
+	FieldErrors    map[string]string
 }
 
 func (v *Validator) Valid() bool {
-	return len(v.FieldErrors) == 0
+	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
 }
 
 func (v *Validator) AddFieldError(key, msg string) {
@@ -31,6 +32,10 @@ func (v *Validator) CheckField(ok bool, key, msg string) {
 	if !ok {
 		v.AddFieldError(key, msg)
 	}
+}
+
+func (v *Validator) AddNonFieldError(msg string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, msg)
 }
 
 func NotBlank(s string) bool {
