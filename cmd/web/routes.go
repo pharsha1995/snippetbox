a31fbd3cf52d2b60aaebbeedf5dafb5e1cfaf985
harsha <pharshavardhan2@gmail.com>
2024-06-02 17:14:07 +0530

    add authorization for routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,21 +12,21 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
 	dynamic := alice.New(app.sessionManager.LoadAndSave)
+	protected := dynamic.Append(app.requireAuthentication)
 	
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
 
 	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
-	mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.snippetCreate))
-	mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
+	mux.Handle("GET /snippet/create", protected.ThenFunc(app.snippetCreate))
+	mux.Handle("POST /snippet/create", protected.ThenFunc(app.snippetCreatePost))
 
 	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.userSignup))
 	mux.Handle("POST /user/signup", dynamic.ThenFunc(app.userSignupPost))
 	mux.Handle("GET /user/login", dynamic.ThenFunc(app.userLogin))
 	mux.Handle("POST /user/login", dynamic.ThenFunc(app.userLoginPost))
-	mux.Handle("POST /user/logout", dynamic.ThenFunc(app.userLogoutPost))
-
-	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+	mux.Handle("POST /user/logout", protected.ThenFunc(app.userLogoutPost))
 
 	return standard.Then(mux)
 }
