8ba5dd3d14b5dff28bd75f990a8049d6172d214c
harsha <pharshavardhan2@gmail.com>
2024-06-02 18:31:34 +0530

    fix case users with deleted account treated authorised cause of session data

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,7 +13,7 @@ func (app *application) routes() http.Handler {
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
-	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf)
+	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 	protected := dynamic.Append(app.requireAuthentication)
 	
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
